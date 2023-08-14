7ad39fad41c70295c965e93f8317ffc508acee11
mcbaby270 <141350887+mcbaby270@users.noreply.github.com>
2023-08-14 09:54:34 +0200

    Add files via upload

---
+++ irebase-config.js
@@ -0,0 +1,16 @@
+// Initialize Firebase
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCq-IaAC5_aCEaQ950gkBgEpbHj_4hUVzI",
+    authDomain: "aquafeeder-a9d68.firebaseapp.com",
+    databaseURL: "https://aquafeeder-a9d68-default-rtdb.firebaseio.com",
+    projectId: "aquafeeder-a9d68",
+    storageBucket: "aquafeeder-a9d68.appspot.com",
+    messagingSenderId: "387880372575",
+    appId: "1:387880372575:web:6858ba479ae87902e18eea",
+    measurementId: "G-J1E31CNE67"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  const database = firebase.database();
+  
---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const feedNowButton = document.getElementById("feedNowButton");
+    const feedTimeInput = document.getElementById("feedTime");
+    const scheduleButton = document.getElementById("scheduleButton");
+  
+    // Handle "Feed Now" button click
+    feedNowButton.addEventListener("click", () => {
+      feedNow();
+    });
+  
+    // Handle "Schedule Feeding" button click
+    scheduleButton.addEventListener("click", () => {
+      const selectedTime = feedTimeInput.value;
+      if (selectedTime) {
+        scheduleFeeding(selectedTime);
+      }
+    });
+  
+    function feedNow() {
+      // Send a command to ESP8266 to trigger feeding immediately
+      // Implement your communication logic here (e.g., using HTTP requests)
+      // Example: Use fetch() to send a request to your ESP8266 endpoint
+      // fetch("ESP8266_FEED_NOW_ENDPOINT");
+    }
+  
+    function scheduleFeeding(time) {
+      // Store the scheduled feeding time in Firebase
+      const scheduledRef = database.ref("feedings").push();
+      scheduledRef.set({
+        time: time
+      });
+    }
+  });
+  
