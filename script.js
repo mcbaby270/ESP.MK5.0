4e8fa553e469f32214a0fe897da69fd9b46e277e
mcbaby270 <141350887+mcbaby270@users.noreply.github.com>
2023-08-21 19:47:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,27 @@
 import firebase from 'firebase/app';
 import 'firebase/database';
-import firebaseConfig from './firebase-config.js'; // Import the Firebase configuration
+import firebaseConfig from './firebase-config.js';
 
-// Initialize Firebase app
 firebase.initializeApp(firebaseConfig);
-
-// Get a reference to the Firebase Realtime Database
 const database = firebase.database();
 
 document.addEventListener("DOMContentLoaded", () => {
-    const feedNowButton = document.getElementById("feedNowButton");
-    const feedTimeInput = document.getElementById("feedTime");
-    const scheduleButton = document.getElementById("scheduleButton");
-  
-    // Handle "Feed Now" button click
-    feedNowButton.addEventListener("click", () => {
-      feedNow();
-    });
-  
-    // Handle "Schedule Feeding" button click
-    scheduleButton.addEventListener("click", () => {
-      const selectedTime = feedTimeInput.value;
-      if (selectedTime) {
-        scheduleFeeding(selectedTime);
-      }
-    });
-  
-    function feedNow() {
-      // Send a command to ESP8266 to trigger feeding immediately
-      // Implement your communication logic here (e.g., using HTTP requests)
-      // Example: Use fetch() to send a request to your ESP8266 endpoint
-      // fetch("ESP8266_FEED_NOW_ENDPOINT");
-    }
-  
-    function scheduleFeeding(time) {
-      // Store the scheduled feeding time in Firebase
+  const feedNowButton = document.getElementById("feedNowButton");
+  const feedTimeInput = document.getElementById("feedTime");
+  const scheduleButton = document.getElementById("scheduleButton");
+
+  feedNowButton.addEventListener("click", feedNow);
+  scheduleButton.addEventListener("click", scheduleFeeding);
+
+  function feedNow() {
+    // Implement your feedNow logic here
+  }
+
+  function scheduleFeeding() {
+    const selectedTime = feedTimeInput.value;
+    if (selectedTime) {
       const scheduledRef = database.ref("feedings").push();
-      scheduledRef.set({
-        time: time
-      });
+      scheduledRef.set({ time: selectedTime });
     }
+  }
 });
