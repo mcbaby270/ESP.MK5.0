c39d1073641a27eacb06643fe1f5a1a281ae6060
mcbaby270 <141350887+mcbaby270@users.noreply.github.com>
2023-08-25 16:43:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
-
-import firebase from 'firebase/app';
-import 'firebase/database'; // Import the Firebase Realtime Database module
-import { firebaseConfig, database } from './firebase-config.js';
-
-firebase.initializeApp(firebaseConfig);
+// script.js
+import { app, database } from './firebase-config.js';
 
 document.addEventListener("DOMContentLoaded", () => {
-  const feedNowButton = document.getElementById("feedNowButton");
-  const feedTimeInput = document.getElementById("feedTime");
-  const scheduleButton = document.getElementById("scheduleButton");
+    const feedNowButton = document.getElementById("feedNowButton");
+    const feedTimeInput = document.getElementById("feedTime");
+    const scheduleButton = document.getElementById("scheduleButton");
 
-  feedNowButton.addEventListener("click", feedNow);
-  scheduleButton.addEventListener("click", scheduleFeeding);
+    feedNowButton.addEventListener("click", feedNow);
+    scheduleButton.addEventListener("click", scheduleFeeding);
 
-  function feedNow() {
-    // Implement your feedNow logic here
-    console.log("Feed Now clicked");
-  }
-
-  function scheduleFeeding() {
-    const selectedTime = feedTimeInput.value;
-    if (selectedTime) {
-      const scheduledRef = database.ref("feedings").push();
-      scheduledRef.set({ time: selectedTime });
-      console.log("Scheduled feeding:", selectedTime);
+    function feedNow() {
+        // Implement your feedNow logic here
+        console.log("Feed Now clicked");
     }
-  }
+
+    function scheduleFeeding() {
+        const selectedTime = feedTimeInput.value;
+        if (selectedTime) {
+            const scheduledRef = push(ref(database, "feedings"));
+            set(scheduledRef, { time: selectedTime });
+            console.log("Scheduled feeding:", selectedTime);
+        }
+    }
 });
+
